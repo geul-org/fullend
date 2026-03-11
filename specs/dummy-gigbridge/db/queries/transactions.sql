6ee0581dcedbcdd09a9b58d88c16cb50e1acdb64
park-jun-woo <mail@parkjunwoo.com>
2026-03-11 14:46:47 +0900

    dummy-gigbridge 정합성 수정: Transaction.Create 추가 + type→tx_type 예약어 회피
    - SSaC: AcceptProposal, ApproveWork에 Transaction.Create 시퀀스 추가
    - DDL/sqlc/OpenAPI: transactions.type → tx_type (Go 예약어 충돌)
    - crosscheck: DDL 미참조 WARNING→ERROR, Scenario 4xx 거부 케이스 스킵
    - billing func TODO 구현
    - go.mod: SSaC 모듈 동기화 (수정지시서 018 Go 예약어 검증)

--- specs/dummy-gigbridge/db/queries/transactions.sql
+++ specs/dummy-gigbridge/db/queries/transactions.sql
@@ -2,5 +2,5 @@
 SELECT * FROM transactions WHERE id = $1;
 
 -- name: TransactionCreate :one
-INSERT INTO transactions (gig_id, type, amount)
+INSERT INTO transactions (gig_id, tx_type, amount)
 VALUES ($1, $2, $3) RETURNING *;
